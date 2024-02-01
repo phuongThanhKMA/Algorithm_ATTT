959cb44e1bf3650815972c84456abf1258bef47e
Phuong Thanh <89241218+phuongThanhKMA@users.noreply.github.com>
2024-02-01 20:10:35 +0700

    Update phepcongmang.py

--- phepcongmang.py
+++ phepcongmang.py
@@ -11,10 +11,10 @@ def phep_cong_mang(A,B,t,W):
     list = [0] * t
     for i in range(t-1,-1,-1):
         x = 2 ** (W)
-        result = (A[i]+B[i]+epsilon)%x
-        list[i]=result
-        if (A[i]+B[i]+epsilon)>=x:
+        list[i]=(A[i]+B[i]+epsilon)
+        if list[i]>=x:
             epsilon = 1
+            list[i]=list[i]%x
         else:
             epsilon = 0
     return epsilon, list
@@ -25,27 +25,30 @@ def phep_cong_mang(A,B,t,W):
 #B=[0,1,226,64]
 def phep_tru_mang(A,B,t,W):
     epsilon = 0
-    result_list = [0] * t
+    list = [0] * t
     for i in range(t-1,-1,-1):
         x = 2 ** (W)
-        result = (A[i]-B[i]-epsilon)%x
-        result_list[i]=result
-        if (A[i]-B[i]-epsilon)<0:
+        list[i] = A[i]-B[i]-epsilon
+        if list[i]<0:
             epsilon = 1
+            list[i]=list[i]%x
         else:
             epsilon = 0
-    return epsilon, result_list
+    return epsilon, list
 
 #print(phep_tru_mang(A,B,t,W))
 
 def cong_tren_F(A,B,F,W):
-    t = math.ceil(math.ceil(math.log2(F))/W) #  Tính toán số lượng phần tử cần thiết trong mảng 
+    t = math.ceil(math.ceil(math.log2(F))/W)
     a = thuattoancoban.so_sang_mang(F,W,A)
     b = thuattoancoban.so_sang_mang(F,W,B)
+    p = thuattoancoban.so_sang_mang(F,W,F)
 
     e,c = phep_cong_mang(a,b,t,W)
-    if e == 1 :
-        e,c = phep_tru_mang(c,2**(t*W),t,W)
+    if e==1:
+        e,c = phep_tru_mang(c,p,t,W)
+    elif e==0:
+        e,c = phep_tru_mang(c,p,t,W)
     return c
 
 def tru_tren_F(A,B,F,W):
@@ -55,5 +58,6 @@ def tru_tren_F(A,B,F,W):
 
     e,c = phep_tru_mang(a,b,t,W)
     if e == 1 :
-        e,c = phep_cong_mang(c,2**(t*W),t,W)
+        p=thuattoancoban.so_sang_mang(F,W,F)
+        e,c = phep_cong_mang(c,p,t,W)
     return c
