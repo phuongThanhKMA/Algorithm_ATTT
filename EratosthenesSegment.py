70593287069afb647d21d84788129a56618281fb
Phuong Thanh <89241218+phuongThanhKMA@users.noreply.github.com>
2024-02-01 20:02:27 +0700

    Update EratosthenesSegment.py
     Đúng với trường hợp tính nguyên tố chạy từ 1-n (1->10).

--- EratosthenesSegment.py
+++ EratosthenesSegment.py
@@ -1,30 +1,16 @@
 def segmented_sieve(limit):
     import math
 
-    def simple_sieve(limit, primes):
-        
-        mark = [True] * (limit + 1)
-        p = 2
-        while p * p <= limit:
-            if mark[p] == True:
-                for i in range(p * p, limit + 1, p):
-                    mark[i] = False
-            p += 1
-
-        for p in range(2, limit + 1):
-            if mark[p]:
-                primes.append(p)
-
-    def sieve_in_range(low, high, primes):
+    def sieve_in_range(low, high):
         limit = int(math.sqrt(high))
-        primes = []
-        simple_sieve(limit, primes)
-        
         n = high - low + 1
         mark = [True] * (n)
-        mark[0] = mark[1] = False 
+        
+        if low < 2:
+            for i in range(1):
+                mark[i] = False
     
-        for prime in primes:
+        for prime in range(2, limit+1):
             low_limit = max(prime * prime, (low + prime - 1) // prime * prime)
             for i in range(low_limit, high + 1, prime):
                 mark[i - low] = False
@@ -32,23 +18,20 @@ def segmented_sieve(limit):
         result = [low + i for i in range(n) if mark[i]]
         return result
 
-    # Find prime numbers between 10 and 50
-    # Xác định phạm vi tìm số nguyên tố
     while True:
         try:
             lower_limit = int(input("Nhập số bắt đầu (số nguyên dương): "))
-            if lower_limit >=0 and lower_limit < user_number:
+            if lower_limit >=0 and lower_limit < limit:
                 break
             else:
                 print("Vui lòng nhập một số nguyên dương trong phạm vi tìm kiếm.")
         except ValueError:
             print("Vui lòng nhập số nguyên dương.")
         
-    # Nhập số kết thúc từ người dùng và đảm bảo là số nguyên dương và lớn hơn số bắt đầu
     while True:
         try:
             upper_limit = int(input("Nhập số kết thúc (số nguyên dương và lớn hơn số bắt đầu): "))
-            if upper_limit > lower_limit and upper_limit <= user_number:
+            if upper_limit > lower_limit and upper_limit <= limit:
                 break
             else:
                 print("Vui lòng nhập một số nguyên dương trong phạm vi tìm kiếm.")
@@ -56,11 +39,10 @@ def segmented_sieve(limit):
             print("Vui lòng nhập số nguyên dương.")
             
 
-    primes_in_range = sieve_in_range(lower_limit, upper_limit, [])
+    primes_in_range = sieve_in_range(lower_limit, upper_limit)
 
     print(f"Prime numbers between {lower_limit} and {upper_limit}: {primes_in_range}")
 
-# Nhập số lượng số nguyên tố muốn tìm từ người dùng
 while True:
     try:
         user_number = int(input("Số giới hạn phạm vi tìm kiếm: "))
